92f23154da4b952b1f2283b586671b8bf9e1ffe8
Daniil <zaklyakov2004@ya.ru>
2020-10-08 17:38:57 +0300

    Game
    Test uploading to git

---
+++ Game Homeless Valera/Homeless Valera/Assets/Scripts/LoadNewScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+
+    public Button beginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beginButton.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+}
